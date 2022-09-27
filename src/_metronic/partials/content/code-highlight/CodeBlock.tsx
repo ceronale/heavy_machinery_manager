de6b53f05802e7293dae197353469969b0e1e3b8
Alexander Ceron <alexander.ceron16@gmail.com>
2022-09-27 19:59:29 -0300

    -

--- src/_metronic/partials/content/code-highlight/CodeBlock.tsx
+++ src/_metronic/partials/content/code-highlight/CodeBlock.tsx
@@ -1,39 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {useState, useEffect, useRef} from 'react'
-import {CopyToClipboard} from 'react-copy-to-clipboard'
-import Highlight, {defaultProps, Language} from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/shadesOfPurple'
-import {OverlayTrigger, Tooltip} from 'react-bootstrap-v5'
+
 
 type Props = {
-  code: string
-  language: Language
+
 }
 
-const CodeBlock: React.FC<Props> = ({code, language}) => {
-  const codeRef = useRef<HTMLDivElement | null>(null)
-  const [copied, setCopied] = useState(false)
-  useEffect(() => {
-    if (!copied) {
-      return
-    }
-
-    setTimeout(() => {
-      setCopied(false)
-    }, 1500)
-  }, [copied])
-
-  useEffect(() => {
-    if (!codeRef.current) {
-      return
-    }
-
-    const prismCodeElement = codeRef.current.querySelector('.prism-code ') as HTMLDivElement
-    if (prismCodeElement) {
-      prismCodeElement.style.background = 'none'
-      prismCodeElement.style.fontSize = '12px'
-    }
-  }, [])
+const CodeBlock: React.FC<Props> = ({}) => {
+  
 
   return (
 
