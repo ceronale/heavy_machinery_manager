c0906f113168b64084327c4bda9a881d33475efc
Alexander Ceron <alexander.ceron16@gmail.com>
2022-09-27 19:51:30 -0300

    -

--- src/_metronic/partials/content/code-highlight/CodeBlock.tsx
+++ src/_metronic/partials/content/code-highlight/CodeBlock.tsx
@@ -36,37 +36,8 @@ const CodeBlock: React.FC<Props> = ({code, language}) => {
   }, [])
 
   return (
-    <div className='py-5'>
-      <div className='highlight'>
-        <OverlayTrigger
-          key='copy-to-clipboard'
-          placement='top'
-          overlay={<Tooltip id='tooltip-copy-to-clipboard'>Copy Code</Tooltip>}
-        >
-          <CopyToClipboard text={code} onCopy={() => setCopied(true)}>
-            <a className='highlight-copy btn'>{copied ? 'copied' : 'copy'}</a>
-          </CopyToClipboard>
-        </OverlayTrigger>
 
-        <div className='highlight-code' ref={codeRef}>
-          <Highlight {...defaultProps} theme={theme} code={code} language={language}>
-            {({className, style, tokens, getLineProps, getTokenProps}) => {
-              return (
-                <pre className={className} style={{maxHeight: '300px', ...style}}>
-                  {tokens.map((line, i) => (
-                    <div {...getLineProps({line, key: i})}>
-                      {line.map((token, key) => (
-                        <span {...getTokenProps({token, key})} />
-                      ))}
-                    </div>
-                  ))}
-                </pre>
-              )
-            }}
-          </Highlight>
-        </div>
-      </div>
-    </div>
+<div ></div>
   )
 }
 
